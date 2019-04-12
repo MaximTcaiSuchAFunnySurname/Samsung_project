8e9714a925907255245c7aa995cb008d5b678bd7
maxim <max@mail.ru>
2019-04-12 21:46:34 +0300

    1)Added Button for adding notes, onClickListener for  that button 2)Changes made in activity_main layout

--- app/src/main/java/com/example/maxim/protov20/MainActivity.java
+++ app/src/main/java/com/example/maxim/protov20/MainActivity.java
@@ -16,77 +16,77 @@ import android.widget.ListView;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-        Button add;
-        Button switchButton;
-        ListView listView;
-        List<String> list, listDeleted;
-        EditText editText;
+    Button add;
+    Button switchButton;
+    ListView listView;
+    List<String> list, listDeleted;
+    EditText editText;
 
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            listView = (ListView) findViewById(R.id.list);
-            switchButton = (Button) findViewById(R.id.switch_button);
-            add = (Button) findViewById(R.id.add);
-            final long[] buttonCTRL = {0};
-            final ArrayAdapter<String> regularList = new ArrayAdapter<String>(this,
-                    android.R.layout.simple_list_item_1, list);
-            final ArrayAdapter<String> deleteList = new ArrayAdapter<String>(this,
-                    android.R.layout.simple_list_item_1, listDeleted);
-            listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-                @Override
-                public boolean onItemLongClick(AdapterView<?> parent,
-                                               View view, int position, long id) {
-                    String temp = list.get(position);
-                    list.remove(position);
-                    listDeleted.add(temp);
-                    regularList.notifyDataSetChanged();
-                    deleteList.notifyDataSetChanged();
-                    return true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.list);
+        switchButton = (Button) findViewById(R.id.switch_button);
+        add = (Button) findViewById(R.id.add);
+        final long[] buttonCTRL = {0};
+        final ArrayAdapter<String> regularList = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, list);
+        final ArrayAdapter<String> deleteList = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, listDeleted);
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent,
+                                           View view, int position, long id) {
+                String temp = list.get(position);
+                list.remove(position);
+                listDeleted.add(temp);
+                regularList.notifyDataSetChanged();
+                deleteList.notifyDataSetChanged();
+                return true;
+            }
+        });
+        switchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (buttonCTRL[0] % 2 == 0) {
+                    listView.setAdapter(regularList);
+                    listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                        @Override
+                        public boolean onItemLongClick(AdapterView<?> parent,
+                                                       View view, int position, long id) {
+                            String temp = list.get(position);
+                            list.remove(position);
+                            listDeleted.add(temp);
+                            regularList.notifyDataSetChanged();
+                            deleteList.notifyDataSetChanged();
+                            return true;
+                        }
+                    });
+                } else {
+                    listView.setAdapter(deleteList);
+                    listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                        @Override
+                        public boolean onItemLongClick(AdapterView<?> parent,
+                                                       View view, int position, long id) {
+                            listDeleted.remove(position);
+                            deleteList.notifyDataSetChanged();
+                            return true;
+                        }
+                    });
                 }
-            });
-            switchButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (buttonCTRL[0] % 2 == 0) {
-                        listView.setAdapter(regularList);
-                        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-                            @Override
-                            public boolean onItemLongClick(AdapterView<?> parent,
-                                                           View view, int position, long id) {
-                                String temp = list.get(position);
-                                list.remove(position);
-                                listDeleted.add(temp);
-                                regularList.notifyDataSetChanged();
-                                deleteList.notifyDataSetChanged();
-                                return true;
-                            }
-                        });
-                    } else {
-                        listView.setAdapter(deleteList);
-                        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-                            @Override
-                            public boolean onItemLongClick(AdapterView<?> parent,
-                                                           View view, int position, long id) {
-                                listDeleted.remove(position);
-                                deleteList.notifyDataSetChanged();
-                                return true;
-                            }
-                        });
-                    }
-                    buttonCTRL[0]++;
-                }
-            });
-            add.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    editText = (EditText) findViewById(R.id.edit_text);
-                    list.add(editText.getText().toString());
-                    regularList.notifyDataSetChanged();
-                    editText.setText("");
-                }
-            });
-        }
+                buttonCTRL[0]++;
+            }
+        });
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText = (EditText) findViewById(R.id.edit_text);
+                list.add(editText.getText().toString());
+                regularList.notifyDataSetChanged();
+                editText.setText("");
+            }
+        });
     }
+}
